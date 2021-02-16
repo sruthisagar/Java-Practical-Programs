7a59a88d11a850e389234abcc9f284da118a5cc0
sruthisagar <sruthidingu@gmail.com>
2021-02-16 10:50:11 +0530

    updated Calculator

--- Lab Questions/src/Calculator.java
+++ Lab Questions/src/Calculator.java
@@ -1,119 +1,89 @@
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.event.*;
-//public class Calculator extends JFrame implements ActionListener{
-//
-//    public static void main(String[] args) {
-//        Calculator a=new Calculator();
-//
-//    }
-//
-//    JTextField t1,t2,t3;
-//    JLabel l;
-//    JButton add,sub,mul,div;
-//    public Calculator(){
-//        t1=new JTextField(20);
-//        t2=new JTextField(20);
-//        add=new JButton("+");
-//        sub=new JButton("-");
-//        mul=new JButton("x");
-//        div=new JButton("/");
-//        l=new JLabel("Result");
-//        add(t1);
-//        add(t2);
-//        add(add);
-//        add(sub);
-//        add(mul);
-//        add(div);
-//        add(l);
-//
-//        add.addActionListener(this);
-//        sub.addActionListener(this);
-//        mul.addActionListener(this);
-//        div.addActionListener(this);
-//        setLayout(new FlowLayout());
-//        setVisible(true);
-//        setSize(400,400);
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    }
-//    public void actionPerformed(ActionEvent ae){
-//        double n1=Double.parseDouble(t1.getText());
-//        double n2=Double.parseDouble(t2.getText());
-//        double val=0;
-//        if(ae.getSource() == add){
-//            val=n1+n2;
-//            l.setText(val+"");
-//        }
-//        if(ae.getSource() == sub){
-//            val=n1-n2;
-//            l.setText(val+"");}
-//        if(ae.getSource() == mul){
-//            val=n1*n2;
-//            l.setText(val+"");}
-//        if(ae.getSource() == div){
-//            if(n2!=0){
-//                val=n1/n2;
-//                l.setText(val+"");}
-//            else {
-//                l.setText("Infinity");
-//            }
-//        }
-//
-//    }
-//}
-
 import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
+        import javax.swing.*;
+        import java.awt.event.*;
 
 public class Calculator extends JFrame implements ActionListener
 {
+    JPanel panel = new JPanel();
     JTextField textField;
-    JButton addn,subn,muln,divn,eqln,clrn,n0,n1,n2,n3,n4,n5,n6,n7,n8,n9;
-    double p=0,q=0;
-    String op;
-    String str1="";
+    JButton add, sub, mul, div, eql, clr, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9;
+    double no1=0, no2=0, result;
+    String operator;
+    String firstDigit = "";
     public Calculator()
     {
-        setLayout(new GridLayout(5,4));
-        textField = new JTextField(20);
-        setBackground(Color.white);
+        setTitle("Calculator");
+        setResizable(false);
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setSize(300,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(new Color(0xffc299));
+        panel.setLayout(new GridLayout(4, 4));
+        textField = new JTextField(15);
         textField.setEditable(false);
         textField.setText("0");
+        textField.setFont(new Font("Courier", Font.BOLD, 30));
 
         n0 = new JButton("0");
+        n0.setFont(new Font("Courier", Font.BOLD, 30));
         n1 = new JButton("1");
+        n1.setFont(new Font("Courier", Font.BOLD, 30));
         n2 = new JButton("2");
+        n2.setFont(new Font("Courier", Font.BOLD, 30));
         n3 = new JButton("3");
+        n3.setFont(new Font("Courier", Font.BOLD, 30));
         n4 = new JButton("4");
+        n4.setFont(new Font("Courier", Font.BOLD, 30));
         n5 = new JButton("5");
+        n5.setFont(new Font("Courier", Font.BOLD, 30));
         n6 = new JButton("6");
+        n6.setFont(new Font("Courier", Font.BOLD, 30));
         n7 = new JButton("7");
+        n7.setFont(new Font("Courier", Font.BOLD, 30));
         n8 = new JButton("8");
+        n8.setFont(new Font("Courier", Font.BOLD, 30));
         n9 = new JButton("9");
-        addn = new JButton("+");
-        subn = new JButton("-");
-        muln = new JButton("*");
-        divn = new JButton("/");
-        eqln = new JButton("=");
-        clrn = new JButton("C");
+        n9.setFont(new Font("Courier", Font.BOLD, 30));
+        add = new JButton("+");
+        add.setFont(new Font("Courier", Font.BOLD, 30));
+        add.setBackground(new Color(0x80b3ff));
+        sub = new JButton("-");
+        sub.setFont(new Font("Courier", Font.BOLD, 30));
+        sub.setBackground(new Color(0x80b3ff));
+        mul = new JButton("*");
+        mul.setFont(new Font("Courier", Font.BOLD, 30));
+        mul.setBackground(new Color(0x80b3ff));
+        div = new JButton("/");
+        div.setFont(new Font("Courier", Font.BOLD, 30));
+        div.setBackground(new Color(0x80b3ff));
+        eql = new JButton("=");
+        eql.setFont(new Font("Courier", Font.BOLD, 30));
+        eql.setBackground(new Color(0x4dffd2));
+        clr = new JButton("C");
+        clr.setFont(new Font("Courier", Font.BOLD, 30));
+        clr.setBackground(new Color(0xff6666));
 
-        add(n0);
-        add(n1);
-        add(n2);
-        add(n3);
-        add(n4);
-        add(n5);
-        add(n6);
-        add(n7);
-        add(n8);
-        add(n9);
-        add(addn);
-        add(subn);
-        add(muln);
-        add(divn);
-        add(eqln);
-        add(clrn);
+
+        panel.add(n1);
+        panel.add(n2);
+        panel.add(n3);
+        panel.add(clr);
+        panel.add(n4);
+        panel.add(n5);
+        panel.add(n6);
+        panel.add(add);
+        panel.add(n7);
+        panel.add(n8);
+        panel.add(n9);
+        panel.add(sub);
+        panel.add(n0);
+        panel.add(mul);
+        panel.add(div);
+        panel.add(eql);
+
+        add(panel);
+        add(textField);
 
         n0.addActionListener(this);
         n1.addActionListener(this);
@@ -125,91 +95,70 @@ public class Calculator extends JFrame implements ActionListener
         n7.addActionListener(this);
         n8.addActionListener(this);
         n9.addActionListener(this);
-        addn.addActionListener(this);
-        subn.addActionListener(this);
-        muln.addActionListener(this);
-        divn.addActionListener(this);
-        eqln.addActionListener(this);
-        clrn.addActionListener(this);
-        add(textField);
+        add.addActionListener(this);
+        sub.addActionListener(this);
+        mul.addActionListener(this);
+        div.addActionListener(this);
+        eql.addActionListener(this);
+        clr.addActionListener(this);
 
-//        setLayout(new FlowLayout());
-        setVisible(true);
-        setSize(300,300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
     }
 
-    public void actionPerformed(ActionEvent ae)
+    public void actionPerformed(ActionEvent e)
     {
-        String str = ae.getActionCommand();
-        if(str.equals("+"))
+        String str = e.getActionCommand();
+        if(str=="+" || str=="-" || str=="*" || str=="/")
         {
-            p=Integer.parseInt(textField.getText());
-            op=str;
-            textField.setText(str1="");
+            no1 = Integer.parseInt(textField.getText());
+            operator = str;
+            firstDigit = "";
+            textField.setText(str);
         }
-        else if(str.equals("-"))
+        else if(str=="=")
         {
-            p=Integer.parseInt(textField.getText());
-            op=str;
-            textField.setText(str1="");
-        }
-        else if(str.equals("*"))
-        {
-            p=Integer.parseInt(textField.getText());
-            op=str;
-            textField.setText(str1="");
-        }
-        else if(str.equals("/"))
-        {
-            p=Integer.parseInt(textField.getText());
-            op=str;
-            textField.setText(str1="");
-        }
-        else if(str.equals("="))
-        {
-            str1="";
-            if(op.equals("+"))
+            firstDigit = "";
+            no2 = Integer.parseInt(textField.getText());
+
+            try
             {
-                q=Integer.parseInt(textField.getText());
-                textField.setText(String.valueOf((p+q)));
+                switch (operator)
+                {
+                    case "+":
+                        result = no1 + no2;
+                        break;
+                    case "-":
+                        result = no1 - no2;
+                        break;
+                    case "*":
+                        result = no1 * no2;
+                        break;
+                    case "/":
+                        result = no1 / no2;
+                        break;
+                }
+                textField.setText((int)no1 + operator + (int)no2 + "=" + result);
             }
-            else if(op.equals("-"))
+            catch (ArithmeticException ex)
             {
-                q=Integer.parseInt(textField.getText());
-                textField.setText(String.valueOf((p-q)));
-            }
-            else if(op.equals("*"))
-            {
-                q=Integer.parseInt(textField.getText());
-                textField.setText(String.valueOf((p*q)));
-            }
-            else if(op.equals("/"))
-            {
-                q=Integer.parseInt(textField.getText());
-                if(q!=0)
-                    textField.setText(String.valueOf((p/q)));
-                else
-                    textField.setText("Undefined");
+                textField.setText("Undefined");
             }
         }
-        else if(str.equals("C"))
+        else if(str=="C")
         {
-            p=0;q=0;
-            textField.setText("");
-            str1="";
             textField.setText("0");
+            firstDigit = "";
+            operator = "";
         }
         else
         {
-            textField.setText(str1.concat(str));
-            str1= textField.getText();
+            textField.setText(firstDigit.concat(str));
+            firstDigit = textField.getText();
         }
     }
 
     public static void main(String args[])
     {
-        Calculator obj=new Calculator();
+        Calculator calculator = new Calculator();
     }
 }
-
